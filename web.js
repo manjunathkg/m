1d37a40f06a9168674a48d40932eb8a776481a28
manjunathkg <mkaliyur@gmail.com>
2013-01-21 14:45:26 -0800

    test

--- web.js
+++ web.js
@@ -1,14 +1,28 @@
-var express = require('express');
+//var express = require('express');
+var send = require('send')
+  , http = require('http');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+
+var app = http.createServer( function(req, res){
+  send(req.url)
+  .from(__dirname + '/app')
+  .maxage(60000)
+  .pipe(res);
+} 
+);
 
 
-app.use("/", express.static(__dirname + '/app'));
-app.use("/modules/*" , express.static(__dirname + '/app/modules'));
-app.use(app.router);
+//app.use("/", express.static(__dirname + '/app'));
+//app.use("/modules/*" , express.static(__dirname + '/app/modules'));
+//app.use(app.router);
  
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+  
