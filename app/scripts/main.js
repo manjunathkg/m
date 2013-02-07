096cb4c68c6582e6a37305b1d0998b40e6e25c53
manjunathkg <mkaliyur@gmail.com>
2013-02-06 22:52:16 -0800

    test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,42 @@
 require.config({
-  shim: {
-  },
+  baseUrl: '..',
+  shim: { 
+          "mainApp" :{
+              deps: ["scripts/vendor/angular", "scripts/app"]
+          } ,
+          "mainController" : {
+              deps : ["scripts/controllers/main"] 
+          } 
+        },
 
   paths: {
+    bootstrap : 'scripts/bootstrap',
     hm: 'vendor/hm',
-    esprima: 'vendor/esprima',
-    jquery: 'vendor/jquery.min'
+    esprima: 'scripts/vendor/esprima',
+    jquery: 'scripts/vendor/jquery.min' , 
+    Console : 'scripts/vendor/console/console',
+    underscore: 'scripts/vendor/underscore/underscore-min',
+    Angular: 'scripts/vendor/angular',
+    mainApp : 'scripts/app',
+    mainController : 'scripts/controllers/main'
   }
 });
+
+
+require(
+  [
+    "require",
+    "Angular",
+    "mainApp",
+    "mainController" 
+  ],
+  function(require){
+      require(["bootstrap"],function(bootstrap){});
+  }
+
+);
  
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
-});
+
+
+
+ 
